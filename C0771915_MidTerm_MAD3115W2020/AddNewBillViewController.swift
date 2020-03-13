1853e4edc33fd23ccb99928b8af30cde2a5d674c
manish <manishpawar94638@gmail.com>
2020-03-13 18:17:49 -0400

    [mp] added more fields to add bill

--- C0771915_MidTerm_MAD3115W2020/AddNewBillViewController.swift
+++ C0771915_MidTerm_MAD3115W2020/AddNewBillViewController.swift
@@ -14,37 +14,35 @@ class AddNewBillViewController: UIViewController {
     
     
 
-    @IBOutlet weak var datePicker: UIDatePicker!
+   
     @IBOutlet weak var agency: UITextField!
     @IBOutlet weak var netProvider: UITextField!
     @IBOutlet weak var billId: UITextField!
     @IBOutlet weak var billDate: UITextField!
-    
     @IBOutlet weak var unitsUsed: UITextField!
     @IBOutlet weak var gbUsed: UITextField!
+    @IBOutlet weak var minutes: UITextField!
+    @IBOutlet weak var billType: UISegmentedControl!
+    @IBOutlet weak var manufacturer: UITextField!
     
-    
+    @IBOutlet weak var mobileNumber: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
     
     
        
        
            override func viewDidLoad() {
         super.viewDidLoad()
-            let datePicker = UIDatePicker()
-            datePicker.datePickerMode = .date
             
-//            pickerData = ["Hydro","Internet","Mobile"]
-//            datePicker.isHidden = true
-//            gbUsed.isHidden = true
-//            unitsUsed.isHidden = true
-//            netProvider.isHidden = true
-//            agency.isHidden = true
+            
+            
         
     }
     
 
    
     @IBAction func biils(_ sender: UISegmentedControl) {
+        
         
     }
     
